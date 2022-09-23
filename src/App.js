a5d1f7e3cf4374fd3b6ec51d90b550dfc01b4d00
habibwebdev <abbasali211@gmail.com>
2022-09-23 06:53:08 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-function App() {
+import React from 'react'
+
+import { Routes, Route } from 'react-router-dom'
+import Footer from './components/Footer'
+import Header from './components/Header'
+
+// import pages
+import Home from './pages/Home'
+import PropertyDetails from './pages/PropertyDetails'
+
+const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="max-w-[1440px] mx-auto bg-white">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/property/:id" element={<PropertyDetails />} />
+      </Routes>
+      <Footer />
     </div>
   )
 }
